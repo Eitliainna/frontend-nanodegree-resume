793b1553e0bd9f1fd55dd9d649cb0ee3a81b95aa
Eitliainna <eitliainna@gmail.com>
2014-11-11 20:13:41 +0200

    added a missing semicolon to line

---
+++ js/resumeBuilder.js
@@ -0,0 +1,6 @@
+var formattedName = HTMLheaderName.replace("%data%", "Danni Shevchuk");
+$("#header").prepend(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").append(formattedRole);
+
